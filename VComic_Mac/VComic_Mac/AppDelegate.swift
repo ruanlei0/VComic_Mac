f698f2fb1681d0ec66f11c2aab30a5be9ac228f2
ruanlei <18801474823@163.com>
2017-06-14 12:06:33 +0800

    新建项目

---
+++ VComic_Mac/VComic_Mac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  VComic_Mac
+//
+//  Created by vcomic on 2017/6/14.
+//  Copyright © 2017年 vcomic. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ VComic_Mac/VComic_Mac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  VComic_Mac
+//
+//  Created by vcomic on 2017/6/14.
+//  Copyright © 2017年 vcomic. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
